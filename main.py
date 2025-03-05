01b5e5d324576c0465971393ed611eb7227feca7
Athanasius <cernoch.p3tr@gmail.com>
2025-03-05 01:09:48 +0100

    Sprite rendering Player movement Groups updatable and drawable

--- main.py
+++ main.py
@@ -1,18 +1,40 @@
 import pygame
 
 from constants import *
+from player import Player
+
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2,
+                    SCREEN_HEIGHT / 2)
+
+
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
+
         screen.fill((0, 0, 0))
+        updatable.update(dt)
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
+
+        dt = clock.tick(FPS) / 1000
 
     print("Starting Asteroids!")
     print("Screen width:", SCREEN_WIDTH)
